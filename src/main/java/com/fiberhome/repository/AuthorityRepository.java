f8c4e1965d191f94b95e973ffb086a249dc71962
zsyin <winsonyin123@gmail.com>
2017-12-08 11:04:52 +0800

    initialize project

---
+++ src/main/java/com/fiberhome/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.fiberhome.repository;
+
+import com.fiberhome.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
